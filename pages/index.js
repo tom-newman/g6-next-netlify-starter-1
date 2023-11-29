4a700cfc610234f946bb3d864c4e57bdb9add7f8
tom-newman <tom_newman@yahoo.com>
2023-11-29 12:14:00 +0000

    1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React, { useEffect, useRef } from 'react';
 
 export default function Home() {
   return (
