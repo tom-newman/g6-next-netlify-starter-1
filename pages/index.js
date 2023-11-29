223cfe289a64d2ef90388ea87747d46c36df75d1
tom-newman <tom_newman@yahoo.com>
2023-11-29 12:11:23 +0000

    revert js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,23 @@
-import React, { useEffect, useRef } from 'react';
-import { Graph } from '@antv/g6';
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-export default () => {
-  const ref = useRef();
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-  const data = {
-    nodes: [
-      { id: 'node1', data: { name: 'Circle1' } },
-      { id: 'node2', data: { name: 'Circle2' } },
-    ],
-    edges: [{ id: 'edge1', source: 'node1', target: 'node2', data: {} }],
-  };
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
 
-  useEffect(() => {
-    if (!ref.current) return;
-
-    const graph = new Graph({
-      container: ref.current,
-      width: 800,
-      height: 500,
-      data,
-      modes: {
-        default: ['drag-node', 'drag-canvas', 'zoom-canvas'],
-      },
-    });
-  }, []);
-
-  return <div ref={ref}></div>;
-};
+      <Footer />
+    </div>
+  )
+}
