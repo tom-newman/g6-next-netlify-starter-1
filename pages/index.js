3db75f0756096fca6b6252c48cb8da2497af5033
tom-newman <tom_newman@yahoo.com>
2023-11-29 11:43:50 +0000

    Added quickstart js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React, { useEffect, useRef } from 'react';
+import { Graph } from '@antv/g6';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default () => {
+  const ref = useRef();
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+  const data = {
+    nodes: [
+      { id: 'node1', data: { name: 'Circle1' } },
+      { id: 'node2', data: { name: 'Circle2' } },
+    ],
+    edges: [{ id: 'edge1', source: 'node1', target: 'node2', data: {} }],
+  };
 
-      <Footer />
-    </div>
-  )
-}
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const graph = new Graph({
+      container: ref.current,
+      width: 800,
+      height: 500,
+      data,
+      modes: {
+        default: ['drag-node', 'drag-canvas', 'zoom-canvas'],
+      },
+    });
+  }, []);
+
+  return <div ref={ref}></div>;
+};
